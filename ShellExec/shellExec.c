0f12fcce01a8522323ab245415fe90be37ef4f43
yekuuun <126786628+Yekuuun@users.noreply.github.com>
2023-08-05 19:55:27 +0200

    update : modifying ShellExec folder struct

--- ShellExec/shellExec.c
+++ ShellExec/shellExec.c
@@ -5,11 +5,12 @@ int main(int argc, char* argv[]){
     printf("Loading ShellExec...\n\n");
 
     if(argc == 1 || argc >= 3){
-        printf("Select one argument <file.exe>");
+        printf("Select one argument file.exe -> PID");
         return EXIT_FAILURE;
     }
 
     //void do something....
+    shellExec(argc);
 
     printf("program runs correctly");
     return EXIT_SUCCESS;
